296cb6769f6fba37b5464a39a27a7ede7b1e7890
Arshan <arshanshaikh97@gmail.com>
2024-11-26 00:32:34 +0530

    Order Model

--- store/models.py
+++ store/models.py
@@ -9,9 +9,21 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_CHOICES = [(MEMBERSHIP_BRONZE, 'Bronze'), ('S', 'Silver'), ('G', 'Gold')]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.IntegerField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [(PAYMENT_STATUS_PENDING, 'Pending'), 
+                              (PAYMENT_STATUS_COMPLETE, 'Complete'),
+                              (PAYMENT_STATUS_FAILED, 'Failed')]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
